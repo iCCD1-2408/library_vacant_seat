03061bd6802065d0eea4f1d59d376862cdac8bc2
kuro1215 <k2210236@gl.cc.uec.ac.jp>
2024-07-17 00:06:18 +0900

    Add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:library_vacant_seat/responsive/desktop_scaffold.dart';
-import 'package:library_vacant_seat/responsive/mobile_scaffold.dart';
-import 'package:library_vacant_seat/responsive/responsive_layout.dart';
-import 'package:library_vacant_seat/responsive/tablet_scaffold.dart';
+import 'package:library_vacant_seat/components/responsive_layout.dart';
+import 'package:library_vacant_seat/pages/floor3.dart';
+import 'package:library_vacant_seat/pages/floor2.dart';
+import 'package:library_vacant_seat/pages/floor1.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
-  runApp(const MainApp());
+  setPathUrlStrategy();
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomePage(),
+      '/floor3': (context) => const Floor3Page(),
+      '/floor2': (context) => const Floor2Page(),
+      '/floor1': (context) => const Floor1Page(),
+      //'/bottom_navigation_bar_screen': (context) => const BottomNavigationBarScreen(),
+      //'/drawer_screen': (context) => const DrawerScreen(),
+    },
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      debugShowCheckedModeBanner: false,
-      home: ResponsiveLayout(
-      mobileScaffold: const MobileScaffold(),
-      tabletScaffold: const TabletScaffold(),
-      desktopScaffold: const DesktopScaffold(),
-      ),
+    return const ResponsiveLayout(
+      Column(
+        children: [
+          Text(
+            'This is Home!',
+            style: TextStyle(fontSize: 30),
+            textAlign: TextAlign.center
+          )
+        ]
+      )
     );
   }
 }
-
